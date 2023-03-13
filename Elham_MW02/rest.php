f20e6c218e8ddaaafd20af68d5747581339af26e
Elham <74769066+emodasser@users.noreply.github.com>
2023-03-13 10:25:53 +0100

    Add files via upload

---
+++ Elham_MW02/rest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*INSERER un nouvel utilisateur dans la base de donnees*/
+
+// $mydb = new PDO('mysql:host=localhost;dbname=drone_elham;charset=utf8','emodasser','modasser');
+// $req = "INSERT INTO utilisateur (nom, prenom, email, naissance, pseudo, mdp) VALUES (?,?,?,?,?,?)";
+// $reqpreparer=$mydb->prepare($req);
+// $tableauDeDonnees=array('Sarron','Christian','sarron.christian@protonmail.com','1970-01-12','login11','mdp11');
+// $reqpreparer->execute($tableauDeDonnees);
+// $reqpreparer->closeCursor();
+
+/*RECUPERER les informations des utilisateurs dans la base de donnees*/
+
+// $mydb = new PDO('mysql:host=localhost;dbname=drone_elham;charset=utf8','emodasser','modasser');
+// $req = "SELECT nom,prenom FROM utilisateur";
+// $reqpreparer=$mydb->prepare($req);
+// $tableauDeDonnees=array();
+// $reqpreparer->execute($tableauDeDonnees);
+// $response=$reqpreparer->fetchAll(PDO::FETCH_ASSOC);
+// print_r($response);
+// $reqpreparer->closeCursor();
+
+/*RECUPERER les donnees du formulaire*/
+
+
+?>
